0a413cea689bd2f35a4aa513301785e493744c77
ismaelsousa <ismaelsousa1998@outlook.com>
2022-10-12 17:17:49 -0300

    feat: add dep react-native-ssl-pinning

--- App.tsx
+++ App.tsx
@@ -1,15 +1,32 @@
 import React, {useEffect, useState} from 'react';
 import {Alert, Image, StyleSheet, Text, View} from 'react-native';
+import {fetch} from 'react-native-ssl-pinning';
 
 const SslPinning: React.FC = () => {
   const [user, setUser] = useState<any>();
 
   useEffect(() => {
-    fetch('https://api.github.com/users/ismaelsousa')
+    fetch('https://api.github.com/users/ismaelsousa', {
+      method: 'GET',
+      pkPinning: true,
+      // disableAllSecurity: true,
+      sslPinning: {
+        certs: ['sha256/r/mIkG3eEpVdm+u/ko/cwxzOMo1bk4TyHIlByibiA5E='],
+      },
+      headers: {
+        Accept: 'application/json; charset=utf-8',
+        'Access-Control-Allow-Origin': '*',
+        e_platform: 'mobile',
+      },
+    })
       .then(async response => setUser(await response.json()))
       .catch(() => Alert.alert('Error', 'Error fetching user'));
   }, []);
 
+  // openssl s_client -servername api.github.com -connect api.github.com:443 | openssl x509 -pubkey -noout | openssl rsa -pubin -outform der | openssl dgst -sha256 -binary | openssl enc -base64
+
+  //openssl s_client -servername api.github.com -connect api.github.com:443 </dev/null | sed -n -e '/-.BEGIN/,/-.END/ p' > mycert.pem
+  //openssl x509 -in mycert.pem -outform der -out mycert.cer
   return (
     <View style={style.container}>
       {user && <Image style={style.img} source={{uri: user.avatar_url}} />}
@@ -18,6 +35,7 @@ const SslPinning: React.FC = () => {
     </View>
   );
 };
+//openssl x509 -in mycert.pem -pubkey -noout | openssl rsa -pubin -outform der | openssl dgst -sha256 -binary | openssl enc -base64
 
 export default SslPinning;
 
