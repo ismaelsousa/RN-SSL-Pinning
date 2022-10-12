2ff5c5179985cd6f8099c0af86f70918a9e3a41e
ismaelsousa <ismaelsousa1998@outlook.com>
2022-10-12 17:32:37 -0300

    feat: remove ssl pinning

--- App.tsx
+++ App.tsx
@@ -1,25 +1,12 @@
 import React, {useEffect, useState} from 'react';
 import {Image, StyleSheet, Text, View} from 'react-native';
-import {fetch} from 'react-native-ssl-pinning';
 
 const SslPinning: React.FC = () => {
   const [show, setShow] = useState<any>();
   const [hasError, setHasError] = useState<boolean>(false);
 
   useEffect(() => {
-    fetch('https://api.tvmaze.com/shows/526', {
-      method: 'GET',
-      pkPinning: true,
-      // disableAllSecurity: true,
-      sslPinning: {
-        certs: ['sha256/c/0nAAuHA5w68X9XBR7OcxMVtuBYAPl9QRcDFgm6+QE='],
-      },
-      headers: {
-        Accept: 'application/json; charset=utf-8',
-        'Access-Control-Allow-Origin': '*',
-        e_platform: 'mobile',
-      },
-    })
+    fetch('https://api.tvmaze.com/shows/526')
       .then(async response => setShow(await response.json()))
       .catch(() => {
         setHasError(true);
@@ -38,7 +25,6 @@ const SslPinning: React.FC = () => {
     </View>
   );
 };
-//openssl x509 -in mycert.pem -pubkey -noout | openssl rsa -pubin -outform der | openssl dgst -sha256 -binary | openssl enc -base64
 
 export default SslPinning;
 
